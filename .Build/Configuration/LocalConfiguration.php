3224abf1f63fd8c739a1506dd60e0dff7a9ab69c
Michiel Roos <michiel@michielroos.com>
2018-09-21 10:17:41 +0200

    Update LocalConfiguration Access host by IP

--- .Build/Configuration/LocalConfiguration.php
+++ .Build/Configuration/LocalConfiguration.php
@@ -16,8 +16,10 @@ return [
     ],
     'EXT' => [
         'extConf' => [
+            'func' => 'a:0:{}',
             'rsaauth' => 'a:1:{s:18:"temporaryDirectory";s:0:"";}',
             'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
+            'wizard_crpagetree' => 'a:0:{}',
         ],
     ],
     'FE' => [
@@ -34,7 +36,6 @@ return [
             'TYPO3\CMS\Install\Updates\BackendUserStartModuleUpdate' => 1,
             'TYPO3\CMS\Install\Updates\Compatibility6ExtractionUpdate' => 1,
             'TYPO3\CMS\Install\Updates\ContentTypesToTextMediaUpdate' => 1,
-            'TYPO3\CMS\Install\Updates\ExtensionManagerTables' => 1,
             'TYPO3\CMS\Install\Updates\FileListInAccessModuleListUpdate' => 1,
             'TYPO3\CMS\Install\Updates\FileListIsStartModuleUpdate' => 1,
             'TYPO3\CMS\Install\Updates\FilesReplacePermissionUpdate' => 1,
@@ -80,5 +81,6 @@ return [
         'systemLogLevel' => 2,
         't3lib_cs_convMethod' => 'mbstring',
         't3lib_cs_utils' => 'mbstring',
+        'trustedHostsPattern' => '.*',
     ],
 ];
