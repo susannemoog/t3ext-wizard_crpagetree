4d5069453b0aed0d7adf972ee8b473cb83948eec
Markus Klein <markus.klein@reelworx.at>
2019-04-23 10:07:45 +0200

    [TASK] Declare dependencies correctly and fix some minor things

--- ext_tables.php
+++ ext_tables.php
@@ -1,29 +1,15 @@
 <?php
+
 defined('TYPO3_MODE') || die('¯\_(ツ)_/¯');
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
 if (TYPO3_MODE === 'BE') {
-    ExtensionManagementUtility::insertModuleFunction(
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::insertModuleFunction(
         'web_func',
         \MichielRoos\WizardCrpagetree\CreatePageTree::class,
         null,
         'LLL:EXT:wizard_crpagetree/Resources/Private/Language/locallang.xml:wiz_crPageTree'
     );
-    ExtensionManagementUtility::addLLrefForTCAdescr(
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
         '_MOD_web_func',
         'EXT:wizard_crpagetree/Resources/Private/Language/ContextSensitiveHelp/default.xml'
     );
