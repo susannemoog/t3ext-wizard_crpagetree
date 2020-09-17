0ed859ebe9fb25afc603a21bcfd10ff67145452b
Markus Klein <markus.klein@reelworx.at>
2020-09-17 16:07:16 +0200

    [TASK] Allow v10 for testing

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.0.0-7.3.0',
-            'typo3' => '8.7.0-9.99.99',
+            'typo3' => '8.7.0-10.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
